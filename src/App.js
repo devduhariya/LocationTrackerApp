ef72a8a557d7ef9e06877519d0925b7021069502
devduhariya <64675676+devduhariya@users.noreply.github.com>
2021-05-10 18:03:09 +0530

    fix error

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Admin from './Components/Admin';
 import AdminMain from './Components/AdminMain';
 import Registration from './Components/Registration';
-import "../node_modules/jquery/dist/jquery.min.js";
-import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "jquery/dist/jquery.min.js";
+// import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 function App() {
   return (
     <div className="App">
