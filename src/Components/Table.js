fae5bd819dd6ea4fde0cdc3bba8b5bbea95a17e3
devduhariya <64675676+devduhariya@users.noreply.github.com>
2021-05-10 16:06:24 +0530

    table fix

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -32,7 +32,7 @@ const Table = (props) => {
                                         <td><button className="btn btn-info" onClick={(ev) => onTrigger(data)}>View</button></td>
                                     </tr>
                                 )
-                            }) : <tr colSpan="5">Loading...</tr>}
+                            }) :'Fetching data'}
                         </tbody>
                     </table>
                     <hr />
